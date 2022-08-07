acdb86b58eecaaf1d580cf7e87486709cbf4aa77
Noahkimtai <noahkimtai24@gmail.com>
2022-08-07 16:56:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let main = document.querySelector('main#main');
+main.remove();
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory')
+newHeader.textContent = "YOUR-NAME is the champion"
+document.body.append(newHeader)
